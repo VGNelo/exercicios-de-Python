ddcdd887c583ea7203372da08099d2202295defe
Gaspar <89266099+VGNelo@users.noreply.github.com>
2025-11-15 14:05:27 -0300

    Add files via upload

---
+++ exercicios 05.py
@@ -0,0 +1,9 @@
+#pip install numpy (instale o Numpy se ainda não tiver no tterminal)
+import numpy as np 
+# cria uma lista
+lista = [1, 2, 3, 4, 5]
+# transforma a lista em um array Numpy
+array = np.array(lista)
+# exibe os dois objetos
+print("Lista: ", lista)
+print("Array: ", array)
